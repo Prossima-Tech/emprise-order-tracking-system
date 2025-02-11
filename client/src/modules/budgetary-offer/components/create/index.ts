a65726ce09eff51a99c8a4d5d54e78a3ec3e0ca1
Ayush Som <ryzensingh@gmail.com>
2025-02-11 17:28:40 +0530

    Rearrangement of files

--- client/src/modules/budgetary-offer/components/create/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as BasicInfoForm } from './BasicInfoForm';
-export { default as WorkItemsForm } from './WorkItemsForm';
-export { default as EMDDetailsForm } from './EMDDetailsForm';
-export { default as ApprovalChainForm } from './ApprovalChainForm';
-export {default as TermsConditionsForm} from './TermsConditionsForm'
