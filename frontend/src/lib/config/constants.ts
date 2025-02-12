4613d611cbde91218e2b62bc8e301ef4bbd74441
Ayush Som <ryzensingh@gmail.com>
2025-02-13 01:32:38 +0530

    frontend changes

--- frontend/src/lib/config/constants.ts
+++ frontend/src/lib/config/constants.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:3000/api';
+export const API_BASE_URL = 'http://ec2-54-157-42-190.compute-1.amazonaws.com/api';
 export const TOKEN_KEY = 'auth_token';
 export const USER_KEY = 'user_data';
 
